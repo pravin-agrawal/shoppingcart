2a14577561b4b53ea580b35eae6c2e221857ae02
pravin <pravin.agrawal@fidelis-inc.com>
2020-04-20 12:03:21 +0530

    code refactored

--- src/main/java/com/interview/shoppingcart/bo/slabInterface.java
+++ src/main/java/com/interview/shoppingcart/bo/slabInterface.java
@@ -12,27 +12,22 @@ public interface slabInterface {
     default Map<Double, List<Integer>> calculateDiscountRate(Map<Double, Integer> discountSlab, Double amount) {
         Map<Double, List<Integer>> amountDiscountMap = new HashMap<>();
         Double lastSlabAmount = 0.0;
-        Integer lastSlabDiscount = 0;
         for(Map.Entry<Double, Integer> entry : discountSlab.entrySet()){
             if(amount > 0){
                 double applicableDiscountAmount = entry.getKey() - lastSlabAmount;
                 if(amount >= applicableDiscountAmount || applicableDiscountAmount == 1.00 ){ //applicableDiscountAmount == 1 means its the last slab
                     lastSlabAmount = entry.getKey();
-                    lastSlabDiscount = entry.getValue();
                     if(amountDiscountMap.containsKey(applicableDiscountAmount)){
                         amountDiscountMap.get(applicableDiscountAmount).add(entry.getValue());
                     }else{
-                        List<Integer> discountList  = new ArrayList<>();
-                        discountList.add(entry.getValue());
-                        amountDiscountMap.put(applicableDiscountAmount == 1 ? amount : applicableDiscountAmount,discountList );
+                        double discountamount = applicableDiscountAmount == 1 ? amount : applicableDiscountAmount;
+                        addDiscountMapEntry(amountDiscountMap, entry, discountamount);
                     }
                 }else{
                     if(amountDiscountMap.containsKey(amount)){
                         amountDiscountMap.get(amount).add(entry.getValue());
                     }else{
-                        List<Integer> discountList  = new ArrayList<>();
-                        discountList.add(entry.getValue());
-                        amountDiscountMap.put(amount,discountList );
+                        addDiscountMapEntry(amountDiscountMap, entry, amount);
                     }
                 }
                 amount = applicableDiscountAmount == 1 ? -1 : amount-applicableDiscountAmount;
@@ -41,10 +36,18 @@ public interface slabInterface {
         return amountDiscountMap;
     }
 
+
+    default void addDiscountMapEntry(Map<Double, List<Integer>> amountDiscountMap, Map.Entry<Double, Integer> entry, double applicableDiscountAmount) {
+        List<Integer> discountList  = new ArrayList<>();
+        discountList.add(entry.getValue());
+        amountDiscountMap.put(applicableDiscountAmount,discountList );
+    }
+
     default Double calculateTotalDiscount(Map<Double, Integer> discountSlab, Double amount){
         Double totalDiscountAmount = 0.0;
         Map<Double, List<Integer>> amountDiscountMap = calculateDiscountRate(discountSlab, amount);
         for(Map.Entry<Double, List<Integer>> entry : amountDiscountMap.entrySet()){
+            //totalDiscountAmount = entry.getValue().stream().map(discountRate -> (discountRate * entry.getKey())/100).mapToDouble(i -> i).sum();
             for(Integer discountRate : entry.getValue()){
                 totalDiscountAmount += (discountRate * entry.getKey())/100;
             }
